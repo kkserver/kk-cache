8c22f8bf5ec51a8e2a0a7b1cf3cdb6a111111c88
zhang hailong <hailongz@qq.com>
2016-12-06 11:48:26 +0800

    update

--- cache/cache.go
+++ cache/cache.go
@@ -3,10 +3,12 @@ package cache
 import (
 	"github.com/kkserver/kk-lib/kk/app"
 	"github.com/kkserver/kk-lib/kk/app/remote"
+	"github.com/kkserver/kk-ping/ping"
 )
 
 type CacheApp struct {
 	app.App
 	Remote *remote.Service
+	Ping   *ping.PingService
 	Cache  *CacheService
 }
